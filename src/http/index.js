8152d14f0d381dca4ba7764d5f2860e2780f3a95
SeungHyo-Lee <sajuni11@naver.com>
2024-03-01 22:26:06 +0900

    공통 axios 처리

--- src/http/index.js
+++ src/http/index.js
@@ -1,5 +1,67 @@
 import axios from 'axios';
+import { useAuthStore } from '@/stores/modules/auth';
 
-axios.defaults.timeout = 5000;
+const http = {};
+const instance = axios.create({
+  timeout: 5000,
+  baseURL: ''
+});
 
-export default axios;
+instance.interceptors.request.use(
+  (config) => {
+    const authStore = useAuthStore();
+    const accessToken = authStore.getAccessToken();
+
+    // if (!accessToken) {
+    //   window.location.href = '/';
+    //   return config;
+    // }
+
+    config.headers['Content-Type'] = 'application/json';
+    config.headers['Authorization'] = `Bearer ${accessToken}`;
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+instance.interceptors.response.use(
+  (res) => res,
+  (error) => Promise.reject(error)
+);
+
+http.get = (url, options) => {
+  return new Promise((resolve, reject) => {
+    instance
+      .get(url, options)
+      .then((res) => {
+        if (res.code == 1) {
+          resolve(res.data);
+        } else {
+          reject(res.msg);
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  });
+};
+
+http.post = (url, data, options) => {
+  return new Promise((resolve, reject) => {
+    instance
+      .post(url, data, options)
+      .then((res) => {
+        if (res.code == 1) {
+          resolve(res.data);
+        } else {
+          reject(res.msg);
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  });
+};
+
+export default http;
