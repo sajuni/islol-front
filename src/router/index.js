7de4149224efc4c84605738d0fe299ad0e5f6cc9
SeungHyo-Lee <sajuni11@naver.com>
2024-03-15 18:33:51 +0900

    라우터 테스트

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,24 @@ import login from '@/router/auth/login';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [...login]
+  routes: [
+    ...login,
+    {
+      path: '/error/404',
+      name: '404',
+      component: () => import('@/views/error/404View.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/error/404'
+    }
+  ]
+});
+
+router.beforeEach((to, from, next) => {
+  console.log(to);
+  console.log(from);
+  next();
 });
 
 export default router;
