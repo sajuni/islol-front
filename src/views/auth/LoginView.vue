72bf9708bd861c7db61e4653b0eb9bd5957ee5fb
SeungHyo-Lee <sajuni11@naver.com>
2024-03-15 19:48:19 +0900

    로그인 페이지 작업중

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,12 +1,23 @@
 <template>
-  <div>테스트</div>
-  <v-card>하하하</v-card>
+  <v-container class="h-100 d-flex">
+    <v-row class="align-center" justify="center">
+      <v-col cols="12" xl="4" lg="6" md="6" sm="8" xs="6">
+        <v-card class="mx-auto px-6 py-8">
+          <v-card-title>Login</v-card-title>
+          <v-form>
+            <v-text-field label="아이디" variant="solo"></v-text-field>
+            <v-text-field label="비밀번호" variant="solo"></v-text-field>
+          </v-form>
+          <v-btn>로그인</v-btn>
+          <v-btn>회원가입</v-btn>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
-import { useCounterStore } from '@/stores/counter';
+// import { useCounterStore } from '@/stores/counter';
 
-const dd = useCounterStore();
-
-dd.test();
+// const dd = useCounterStore();
 </script>
